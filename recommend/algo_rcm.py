915bc77cb18732183191382e5cb08caeb5e69851
manhbd-22022602 <22022602@vnu.edu.vn>
2024-05-24 17:02:30 +0700

    Payment authorize

--- recommend/algo_rcm.py
+++ recommend/algo_rcm.py
@@ -49,24 +49,37 @@ class RecentRecommender:
         self.user = user
         self.co_matrix_generator = CoOccurrenceMatrixGenerator()
         self.co_occurrence_matrix = self.co_matrix_generator.get_or_create_matrix()
+        self.model = None
+        self.mlb = MultiLabelBinarizer()
+        self.movies_df = pd.DataFrame(list(Movie.objects.all().values('id', 'title', 'genre')))
+        if self.model is None:
+            self.train_knn_model()
+
+    def preprocess_genres(self):
+        self.movies_df['genre'] = self.movies_df['genre'].apply(lambda x: x.split(', '))
+        self.genre_matrix = self.mlb.fit_transform(self.movies_df['genre'])
+
+    def train_knn_model(self):
+        if self.model is None:
+            self.preprocess_genres()
+            self.model = NearestNeighbors(metric='euclidean', algorithm='brute')
+            self.model.fit(self.genre_matrix)
 
     def recommend(self, top_n=18):
         watched_movies = self._get_watched_movies()
-        print(watched_movies)
-        if len(watched_movies) == 0:
+        if not watched_movies:
             return self._get_popular_movies(top_n)
-        if len(watched_movies) > 1000:  # Adjust this threshold as needed
-            watched_movies = watched_movies[:1000]
-        recommendations = self._generate_recommendations(watched_movies)
-        if not recommendations:
-            return self._get_popular_movies(top_n)
-        return self._get_top_n_recommendations(recommendations, top_n)
+        watched_movies = watched_movies[:1000] if len(watched_movies) > 1000 else watched_movies
+        combined_recommendations = self._combine_recommendations(
+            self._generate_co_occurrence_recommendations(watched_movies),
+            self._generate_knn_recommendations(watched_movies, top_n)
+        )
+        return self._get_top_n_recommendations(combined_recommendations, top_n)
 
     def _get_watched_movies(self):
-        watched = MyList.objects.filter(user=self.user, watch=True).values_list('movie_id', flat=True)
-        return list(watched)
+        return list(MyList.objects.filter(user=self.user, watch=True).values_list('movie_id', flat=True))
 
-    def _generate_recommendations(self, watched_movies):
+    def _generate_co_occurrence_recommendations(self, watched_movies):
         recommendations = {}
         for movie in watched_movies:
             if movie in self.co_occurrence_matrix:
@@ -75,17 +88,29 @@ class RecentRecommender:
                         recommendations[co_movie] = recommendations.get(co_movie, 0) + score
         return recommendations
 
+    def _generate_knn_recommendations(self, watched_movies, top_n):
+        recommendations = []
+        for movie_id in watched_movies:
+            movie = Movie.objects.get(id=movie_id)
+            genre_list = movie.genre.split(', ')
+            genre_vector = self.mlb.transform([genre_list])
+            _, indices = self.model.kneighbors(genre_vector, n_neighbors=top_n + 1)
+            recommendations.extend(self.movies_df.iloc[indices[0]]['id'].tolist()[1:])
+        return recommendations
+
+    def _combine_recommendations(self, co_occurrence_recommendations, knn_recommendations):
+        for movie in knn_recommendations:
+            co_occurrence_recommendations[movie] = co_occurrence_recommendations.get(movie, 0) + 1
+        return co_occurrence_recommendations
+
     def _get_top_n_recommendations(self, recommendations, top_n):
         sorted_recommendations = sorted(recommendations.items(), key=lambda item: item[1], reverse=True)
-        top_recommendations = sorted_recommendations[:top_n]
-        top_movie_ids = [movie_id for movie_id, score in top_recommendations]
+        top_movie_ids = [movie_id for movie_id, _ in sorted_recommendations[:top_n]]
         return Movie.objects.filter(id__in=top_movie_ids)
 
     def _get_popular_movies(self, top_n):
-        # Gợi ý phim theo số lượt xem trong bảng MyList
         trending_movie_ids = MyList.objects.values('movie').annotate(total_watch=Count('movie')).order_by('-total_watch')[:12]
-        trending_movies = Movie.objects.filter(id__in=[movie['movie'] for movie in trending_movie_ids])
-        return trending_movies
+        return Movie.objects.filter(id__in=[movie['movie'] for movie in trending_movie_ids])
     
 # To get similar movies based on user rating
 def get_similar(movie_name,rating,corrMatrix):
