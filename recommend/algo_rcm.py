e8f1a8466e417f46243ffacac6c8f5b016c9b560
manhbd-22022602 <22022602@vnu.edu.vn>
2024-05-25 15:07:12 +0700

    Refactor code

--- recommend/algo_rcm.py
+++ recommend/algo_rcm.py
@@ -6,7 +6,7 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
 from scipy.sparse import coo_matrix
-from .models import Movie, MyList, Myrating, User
+from .models import Products, WatchHistory, ProductRating, User
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from django.db.models import Q, Avg, Count, Case, When
@@ -16,16 +16,16 @@ import pickle
 
 class SearchEngineRecommender:
     def __init__(self):
-        self.movies_df = pd.DataFrame(list(Movie.objects.all().values('id', 'title', 'genre')))
+        self.products_df = pd.DataFrame(list(Products.objects.all().values('id', 'title', 'genre')))
         self.model = None
         self.mlb = MultiLabelBinarizer()
 
     def preprocess_genres(self):
-        if not isinstance(self.movies_df['genre'][0], list):
+        if not isinstance(self.products_df['genre'][0], list):
             # Tách các thể loại cách nhau bằng dấu phẩy
-            self.movies_df['genre'] = self.movies_df['genre'].apply(lambda x: x.split(', '))
+            self.products_df['genre'] = self.products_df['genre'].apply(lambda x: x.split(', '))
 
-        self.genre_matrix = self.mlb.fit_transform(self.movies_df['genre'])
+        self.genre_matrix = self.mlb.fit_transform(self.products_df['genre'])
 
 
     def train_model(self):
@@ -58,10 +58,10 @@ class SearchEngineRecommender:
 
         distances, indices = self.model.kneighbors(genre_vector, n_neighbors=k+1)
 
-        # Get recommended movie ids
-        recommended_movie_ids = self.movies_df.iloc[indices[0]]['id'].tolist()
+        # Get recommended product ids
+        recommended_product_ids = self.products_df.iloc[indices[0]]['id'].tolist()
 
-        return recommended_movie_ids[1:]
+        return recommended_product_ids[1:]
 
 class RecentRecommender:
     def __init__(self, user):
@@ -70,13 +70,13 @@ class RecentRecommender:
         self.co_occurrence_matrix = self.co_matrix_generator.get_or_create_matrix()
         self.model = None
         self.mlb = MultiLabelBinarizer()
-        self.movies_df = pd.DataFrame(list(Movie.objects.all().values('id', 'title', 'genre')))
+        self.products_df = pd.DataFrame(list(Products.objects.all().values('id', 'title', 'genre')))
         if self.model is None:
             self.load_cached_knn_model()
 
     def preprocess_genres(self):
-        self.movies_df['genre'] = self.movies_df['genre'].apply(lambda x: x.split(', '))
-        self.genre_matrix = self.mlb.fit_transform(self.movies_df['genre'])
+        self.products_df['genre'] = self.products_df['genre'].apply(lambda x: x.split(', '))
+        self.genre_matrix = self.mlb.fit_transform(self.products_df['genre'])
 
     def load_cached_knn_model(self):
         cached_model = CachedModel.objects.last()
@@ -94,60 +94,60 @@ class RecentRecommender:
             self.model.fit(self.genre_matrix)
 
     def recommend(self, top_n=18):
-        watched_movies = self._get_watched_movies()
-        if not watched_movies:
-            return self._get_popular_movies(top_n)
-        watched_movies = watched_movies[:10] if len(watched_movies) > 10 else watched_movies
+        watched_products = self._get_watched_products()
+        if not watched_products:
+            return self._get_popular_products(top_n)
+        watched_products = watched_products[:10] if len(watched_products) > 10 else watched_products
         combined_recommendations = self._combine_recommendations(
-            self._generate_co_occurrence_recommendations(watched_movies),
-            self._generate_knn_recommendations(watched_movies, top_n)
+            self._generate_co_occurrence_recommendations(watched_products),
+            self._generate_knn_recommendations(watched_products, top_n)
         )
         return self._get_top_n_recommendations(combined_recommendations, top_n)
 
-    def _get_watched_movies(self):
-        return list(MyList.objects.filter(user=self.user, watch=True).values_list('movie_id', flat=True))
+    def _get_watched_products(self):
+        return list(WatchHistory.objects.filter(user=self.user, watch=True).values_list('product_id', flat=True))
 
-    def _generate_co_occurrence_recommendations(self, watched_movies):
+    def _generate_co_occurrence_recommendations(self, watched_products):
         recommendations = {}
-        for movie in watched_movies:
-            if movie in self.co_occurrence_matrix:
-                for co_movie, score in self.co_occurrence_matrix[movie].items():
-                    if co_movie not in watched_movies:
-                        recommendations[co_movie] = recommendations.get(co_movie, 0) + score
+        for product in watched_products:
+            if product in self.co_occurrence_matrix:
+                for co_product, score in self.co_occurrence_matrix[product].items():
+                    if co_product not in watched_products:
+                        recommendations[co_product] = recommendations.get(co_product, 0) + score
         return recommendations
 
-    def _generate_knn_recommendations(self, watched_movies, top_n):
+    def _generate_knn_recommendations(self, watched_products, top_n):
         recommendations = []
-        for movie_id in watched_movies:
-            movie = Movie.objects.get(id=movie_id)
-            genre_list = movie.genre.split(', ')
+        for product_id in watched_products:
+            product = Products.objects.get(id=product_id)
+            genre_list = product.genre.split(', ')
             genre_vector = self.mlb.transform([genre_list])
             _, indices = self.model.kneighbors(genre_vector, n_neighbors=top_n + 1)
-            recommendations.extend(self.movies_df.iloc[indices[0]]['id'].tolist()[1:])
+            recommendations.extend(self.products_df.iloc[indices[0]]['id'].tolist()[1:])
         return recommendations
 
     def _combine_recommendations(self, co_occurrence_recommendations, knn_recommendations):
-        for movie in knn_recommendations:
-            co_occurrence_recommendations[movie] = co_occurrence_recommendations.get(movie, 0) + 1
+        for product in knn_recommendations:
+            co_occurrence_recommendations[product] = co_occurrence_recommendations.get(product, 0) + 1
         return co_occurrence_recommendations
 
     def _get_top_n_recommendations(self, recommendations, top_n):
         sorted_recommendations = sorted(recommendations.items(), key=lambda item: item[1], reverse=True)
-        top_movie_ids = [movie_id for movie_id, _ in sorted_recommendations[:top_n]]
-        return Movie.objects.filter(id__in=top_movie_ids)
+        top_product_ids = [product_id for product_id, _ in sorted_recommendations[:top_n]]
+        return Products.objects.filter(id__in=top_product_ids)
 
-    def _get_popular_movies(self, top_n):
-        trending_movie_ids = MyList.objects.values('movie').annotate(total_watch=Count('movie')).order_by('-total_watch')[:12]
-        return Movie.objects.filter(id__in=[movie['movie'] for movie in trending_movie_ids])
+    def _get_popular_products(self, top_n):
+        trending_product_ids = WatchHistory.objects.values('product').annotate(total_watch=Count('product')).order_by('-total_watch')[:12]
+        return Products.objects.filter(id__in=[product['product'] for product in trending_product_ids])
     
-# To get similar movies based on user rating
-def get_similar(movie_name,rating,corrMatrix):
-    similar_ratings = corrMatrix[movie_name]*(rating-2.5)
+# To get similar products based on user rating
+def get_similar(product_name,rating,corrMatrix):
+    similar_ratings = corrMatrix[product_name]*(rating-2.5)
     similar_ratings = similar_ratings.sort_values(ascending=False)
     return similar_ratings
 
-def get_trending_movies():
+def get_trending_products():
     # Gợi ý phim theo số lượt xem trong bảng MyList
-    trending_movie_ids = MyList.objects.values('movie').annotate(total_watch=Count('movie')).order_by('-total_watch')[:12]
-    trending_movies = Movie.objects.filter(id__in=[movie['movie'] for movie in trending_movie_ids])
-    return trending_movies
+    trending_product_ids = WatchHistory.objects.values('product').annotate(total_watch=Count('product')).order_by('-total_watch')[:12]
+    trending_products = Products.objects.filter(id__in=[product['product'] for product in trending_product_ids])
+    return trending_products
--- recommend/products_view.py
+++ recommend/products_view.py
@@ -6,59 +6,59 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect, Http404
 from django.core.paginator import Paginator
 from django.db.models import Q, Avg
-from .models import Movie, Myrating, MyList, Comment, Report
+from .models import Products, ProductRating, WatchHistory, Comment, Report
 
-class MovieDetailView(LoginRequiredMixin, DetailView):
-    model = Movie
+class productDetailView(LoginRequiredMixin, DetailView):
+    model = Products
     template_name = 'recommend/detail.html'
-    context_object_name = 'movie'
+    context_object_name = 'product'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        movie_id = self.kwargs['pk']
-        movie = self.get_object()
+        product_id = self.kwargs['pk']
+        product = self.get_object()
         user = self.request.user
 
-        if not user.userprofile.is_vip and movie.is_vip:
-            raise Http404("Only VIP members can access this movie.")
+        if not user.userprofile.is_vip and product.is_vip:
+            raise Http404("Only VIP members can access this product.")
 
         try:
-            watch_item = MyList.objects.get(movie_id=movie_id, user=user)
+            watch_item = WatchHistory.objects.get(product_id=product_id, user=user)
             context['update'] = watch_item.watch
-        except MyList.DoesNotExist:
+        except WatchHistory.DoesNotExist:
             context['update'] = False
 
-        user_rating = Myrating.objects.filter(movie_id=movie_id, user=user).first()
-        context['movie_rating'] = user_rating.rating if user_rating else 0
+        user_rating = ProductRating.objects.filter(product_id=product_id, user=user).first()
+        context['product_rating'] = user_rating.rating if user_rating else 0
         context['rate_flag'] = user_rating is not None
 
-        context['comments'] = Comment.objects.filter(movie=movie).order_by('-created_at')
-        context['movie_avg_rating'] = Myrating.objects.filter(movie_id=movie_id).aggregate(Avg('rating'))['rating__avg'] or 0
+        context['comments'] = Comment.objects.filter(product=product).order_by('-created_at')
+        context['product_avg_rating'] = ProductRating.objects.filter(product_id=product_id).aggregate(Avg('rating'))['rating__avg'] or 0
 
         return context
 
     def post(self, request, *args, **kwargs):
-        movie = self.get_object()
+        product = self.get_object()
         user = request.user
 
         if 'watch' in request.POST:
             watch_flag = request.POST['watch']
             update = watch_flag == 'on'
-            mylist, created = MyList.objects.get_or_create(movie_id=movie.id, user=user)
+            mylist, created = WatchHistory.objects.get_or_create(product_id=product.id, user=user)
             mylist.watch = update
             mylist.save()
-            messages.success(request, "Movie added to your list!" if update else "Movie removed from your list!")
+            messages.success(request, "product added to your list!" if update else "product removed from your list!")
 
         elif 'rating' in request.POST:
             rate = request.POST['rating']
-            rating, created = Myrating.objects.get_or_create(movie_id=movie.id, user=user)
+            rating, created = ProductRating.objects.get_or_create(product_id=product.id, user=user)
             rating.rating = rate
             rating.save()
             messages.success(request, "Rating has been submitted!")
 
         elif 'comment' in request.POST:
             comment_text = request.POST['comment']
-            Comment.objects.create(user=user, movie=movie, text=comment_text)
+            Comment.objects.create(user=user, product=product, text=comment_text)
             messages.success(request, "Comment has been submitted!")
 
         elif 'report' in request.POST:
@@ -72,35 +72,34 @@ class MovieDetailView(LoginRequiredMixin, DetailView):
 
 class WatchListView(LoginRequiredMixin, ListView):
     template_name = 'recommend/watch.html'
-    context_object_name = 'movies'
+    context_object_name = 'products'
 
     def get_queryset(self):
         user = self.request.user
         query = self.request.GET.get('q')
-        if user.userprofile.is_vip:
-            movies = Movie.objects.filter(mylist__watch=True, mylist__user=user)
-        else:
-            movies = Movie.objects.filter(mylist__watch=True, mylist__user=user, is_vip=False)
+        watch_history = WatchHistory.objects.filter(user=user, watch=True).select_related('product')
+
+        products = [watch.product for watch in watch_history]
 
         if query:
-            movies = movies.filter(Q(title__icontains=query)).distinct()
+            products = products.filter(Q(title__icontains=query)).distinct()
 
-        return movies
+        return products
 
-class MovieFilterView(View):
+class productFilterView(View):
     def get(self, request, *args, **kwargs):
         genre = request.GET.get('genre')
-        movies = Movie.objects.all()
+        products = Products.objects.all()
 
         if genre:
-            movies = movies.filter(genre__icontains=genre)
+            products = products.filter(genre__icontains=genre)
 
-        paginator = Paginator(movies, 20)
+        paginator = Paginator(products, 20)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
         context = {
-            'movies': page_obj,
+            'products': page_obj,
         }
 
-        return render(request, 'recommend/movie_filter.html', context)
+        return render(request, 'recommend/product_filter.html', context)
